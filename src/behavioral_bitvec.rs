ac9ca231b5f4c85064569d67361ff7a2b98947c6
Michael Neumann <mneumann@ntecs.de>
2016-03-12 21:54:32 +0100

    Add hamming distance and test cases

--- src/behavioral_bitvec.rs
+++ src/behavioral_bitvec.rs
@@ -1,4 +1,5 @@
 use primal_bit::BitVec;
+use hamming;
 
 pub struct BehavioralBitvec {
     bitvec: BitVec,
@@ -13,6 +14,10 @@ impl BehavioralBitvec {
         }
     }
 
+    pub fn hamming_distance(&self, other: &Self) -> u64 {
+        hamming::distance_fast(self.bitvec.as_bytes(), other.bitvec.as_bytes()).unwrap()
+    }
+
     #[inline]
     pub fn push(&mut self, output: f64) {
         if output >= 0.0 {
@@ -22,3 +27,29 @@ impl BehavioralBitvec {
     }
 }
 
+#[test]
+fn test_behavioral_bitvec() {
+   let mut a = BehavioralBitvec::new(3);
+   let mut b = BehavioralBitvec::new(3);
+
+   // [1, 1, 0]
+   a.push(1.0);
+   a.push(2.0);
+   a.push(-1.0);
+   let ba: Vec<_> = a.bitvec.iter().collect();
+
+   // [0, 1, 1]
+   b.push(-1.0);
+   b.push(1.0);
+   b.push(1.0);
+   let bb: Vec<_> = b.bitvec.iter().collect();
+
+   assert_eq!(vec![true, true, false], ba);
+   assert_eq!(vec![false, true, true], bb);
+
+   let d1 = a.hamming_distance(&b);
+   let d2 = b.hamming_distance(&a);
+
+   assert_eq!(d1, d2);
+   assert_eq!(2, d1);
+}
