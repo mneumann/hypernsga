96ee4e716ada8ff7715c7f1c08bb45f8870d18d3
Michael Neumann <mneumann@ntecs.de>
2016-08-18 11:32:20 +0200

    Factor out imgui gui() function

---
+++ hypernsga-gui/src/imgui_ui.rs
@@ -0,0 +1,245 @@
+use super::{State, Action, ViewMode};
+use nsga2::population::{RankedPopulation};
+use hypernsga::fitness::{Fitness};
+use hypernsga::cppn::{G};
+use imgui::*;
+
+pub fn gui<'a>(ui: &Ui<'a>, state: &mut State, population: &RankedPopulation<G, Fitness>) {
+    ui.window(im_str!("Evolutionary Graph Optimization"))
+        .size((300.0, 100.0), ImGuiSetCond_FirstUseEver)
+        .build(|| {
+            // if ui.collapsing_header(im_str!("General")).build() {
+            ui.text(im_str!("Iteration: {}", state.iteration));
+            ui.text(im_str!("Best Fitness: {:.3}", state.best_fitness));
+            ui.separator();
+
+            if state.running {
+                if ui.small_button(im_str!("STOP")) {
+                    state.running = false;
+                }
+            } else {
+                if ui.small_button(im_str!("START")) {
+                    state.running = true;
+                }
+            }
+
+            if ui.collapsing_header(im_str!("General")).build() {
+                if ui.small_button(im_str!("Recalc Fitness")) {
+                    state.recalc_fitness = true;
+                }
+                if ui.small_button(im_str!("Export Best")) {
+                    state.action = Action::ExportBest;
+                }
+                if ui.small_button(im_str!("Reset")) {
+                    state.action = Action::ResetNet;
+                }
+
+                ui.slider_float(im_str!("Stop when Fitness"),
+                                  &mut state.stop_when_fitness_above,
+                                  0.9,
+                                  1.0)
+                    .build();
+                ui.checkbox(im_str!("Enable Autoreset"), &mut state.auto_reset_enable);
+                ui.slider_int(im_str!("Autoreset after"),
+                                &mut state.auto_reset,
+                                100,
+                                10000)
+                    .build();
+                ui.text(im_str!("Autoreset counter: {}", state.auto_reset_counter));
+
+                let views = &[im_str!("detailed"),
+                              im_str!("multi cppn"),
+                              im_str!("multi graph"),
+                              im_str!("overview")];
+                let mut current: i32 = match state.view {
+                    ViewMode::BestDetailed => 0,
+                    ViewMode::CppnOverview => 1,
+                    ViewMode::GraphOverview => 2,
+                    ViewMode::Overview => 3,
+                };
+                if ui.combo(im_str!("view"), &mut current, views, 4) {
+                    if current == 0 {
+                        state.view = ViewMode::BestDetailed;
+                    } else if current == 1 {
+                        state.view = ViewMode::CppnOverview;
+                    } else if current == 2 {
+                        state.view = ViewMode::GraphOverview;
+                    } else if current == 3 {
+                        state.view = ViewMode::Overview;
+                    }
+                }
+            }
+
+            if ui.collapsing_header(im_str!("Population Metrics")).build() {
+                let best_domain = population.individuals()
+                    .iter()
+                    .max_by_key(|ind| (ind.fitness().domain_fitness * 1_000_000.0) as usize)
+                    .unwrap();
+                let worst_domain = population.individuals()
+                    .iter()
+                    .min_by_key(|ind| (ind.fitness().domain_fitness * 1_000_000.0) as usize)
+                    .unwrap();
+                ui.text(im_str!("Best Domain Fitness: {:.3}",
+                                best_domain.fitness().domain_fitness));
+                ui.text(im_str!("Age of Best: {}", best_domain.genome().age(state.iteration)));
+
+                ui.text(im_str!("Worst Domain Fitness: {:.3}",
+                                worst_domain.fitness().domain_fitness));
+                ui.text(im_str!("Age of Worst: {}",
+                                worst_domain.genome().age(state.iteration)));
+            }
+
+            if ui.collapsing_header(im_str!("History")).build() {
+                let fitness_histogram: Vec<_> =
+                    state.best_fitness_history.iter().map(|&(_i, f)| f as f32).collect();
+                PlotLines::new(im_str!("performance"), &fitness_histogram)
+                    .overlay_text(im_str!("Domain Fitness"))
+                    .graph_size(ImVec2::new(400.0, 50.0))
+                    .scale_min(0.0)
+                    .scale_max(1.0)
+                    .build();
+            }
+            if ui.collapsing_header(im_str!("Objectives")).build() {
+                ui.checkbox(im_str!("Behavioral Diversity"),
+                            &mut state.objectives_use_behavioral);
+                ui.checkbox(im_str!("Connection Cost"), &mut state.objectives_use_cct);
+                ui.checkbox(im_str!("Age Diversity"), &mut state.objectives_use_age);
+                ui.checkbox(im_str!("Saturation"), &mut state.objectives_use_saturation);
+                ui.checkbox(im_str!("Complexity"), &mut state.objectives_use_complexity);
+            }
+            if ui.collapsing_header(im_str!("Population Settings")).build() {
+                ui.slider_int(im_str!("Population Size"), &mut state.mu, state.k, 1000).build();
+                ui.slider_int(im_str!("Offspring Size"), &mut state.lambda, 1, 1000).build();
+            }
+
+            if ui.collapsing_header(im_str!("Selection")).build() {
+                ui.slider_int(im_str!("Tournament Size"), &mut state.k, 1, state.mu).build();
+                ui.slider_float(im_str!("NSGP Objective Epsilon"),
+                                  &mut state.nsgp_objective_eps,
+                                  0.0,
+                                  1.0)
+                    .build();
+            }
+
+            if ui.collapsing_header(im_str!("View")).build() {
+                ui.slider_float(im_str!("Rotate Substrate x"),
+                                  &mut state.rotate_substrate_x,
+                                  0.0,
+                                  360.0)
+                    .build();
+                ui.slider_float(im_str!("Rotate Substrate y"),
+                                  &mut state.rotate_substrate_y,
+                                  0.0,
+                                  360.0)
+                    .build();
+                ui.slider_float(im_str!("Rotate Substrate z"),
+                                  &mut state.rotate_substrate_z,
+                                  0.0,
+                                  360.0)
+                    .build();
+                ui.slider_float(im_str!("Scale Substrate x"),
+                                  &mut state.scale_substrate_x,
+                                  0.0,
+                                  1.0)
+                    .build();
+                ui.slider_float(im_str!("Scale Substrate y"),
+                                  &mut state.scale_substrate_y,
+                                  0.0,
+                                  1.0)
+                    .build();
+                ui.slider_float(im_str!("Scale Substrate z"),
+                                  &mut state.scale_substrate_z,
+                                  0.0,
+                                  1.0)
+                    .build();
+            }
+
+            if ui.collapsing_header(im_str!("CPPN")).build() {
+                ui.slider_float(im_str!("Link Expression Min"),
+                                  &mut state.link_expression_min,
+                                  -1.0,
+                                  1.0)
+                    .build();
+                ui.slider_float(im_str!("Link Expression Max"),
+                                  &mut state.link_expression_max,
+                                  -1.0,
+                                  1.0)
+                    .build();
+
+                ui.slider_float(im_str!("Link Weight Range (bipolar)"),
+                                  &mut state.link_weight_range,
+                                  0.1,
+                                  5.0)
+                    .build();
+                ui.slider_float(im_str!("Link Weight Creation Sigma"),
+                                  &mut state.link_weight_creation_sigma,
+                                  0.01,
+                                  1.0)
+                    .build();
+                ui.slider_float(im_str!("Weight Perturbance Sigma"),
+                                  &mut state.weight_perturbance_sigma,
+                                  0.0,
+                                  1.0)
+                    .build();
+            }
+
+            if ui.collapsing_header(im_str!("Neighbor Matching")).build() {
+                ui.checkbox(im_str!("Edge Weight Scoring"), &mut state.nm_edge_score);
+                ui.slider_int(im_str!("Iterations"), &mut state.nm_iters, 1, 1000).build();
+                ui.slider_float(im_str!("Eps"), &mut state.nm_eps, 0.0, 1.0).build();
+            }
+
+            if ui.collapsing_header(im_str!("Mutation")).build() {
+                ui.slider_float(im_str!("Mutation Rate"),
+                                  &mut state.mutate_element_prob,
+                                  0.0,
+                                  1.0)
+                    .build();
+                ui.slider_int(im_str!("Weights"), &mut state.mutate_weights, 1, 100).build();
+                ui.slider_int(im_str!("Add Node"), &mut state.mutate_add_node, 0, 100).build();
+                ui.slider_int(im_str!("Drop Node"), &mut state.mutate_drop_node, 0, 100).build();
+                ui.slider_int(im_str!("Modify Node"),
+                                &mut state.mutate_modify_node,
+                                0,
+                                100)
+                    .build();
+                ui.slider_int(im_str!("Connect"), &mut state.mutate_connect, 0, 100).build();
+                ui.slider_int(im_str!("Disconnect"), &mut state.mutate_disconnect, 0, 100).build();
+                ui.slider_int(im_str!("Sym Join"),
+                                &mut state.mutate_symmetric_join,
+                                0,
+                                100)
+                    .build();
+                ui.slider_int(im_str!("Sym Fork"),
+                                &mut state.mutate_symmetric_fork,
+                                0,
+                                100)
+                    .build();
+                ui.slider_int(im_str!("Sym Connect"),
+                                &mut state.mutate_symmetric_connect,
+                                0,
+                                100)
+                    .build();
+            }
+            if ui.collapsing_header(im_str!("Global Mutation")).build() {
+                ui.slider_float(im_str!("Global Mutation Rate"),
+                                  &mut state.global_mutation_rate,
+                                  0.0,
+                                  1.0)
+                    .build();
+                ui.slider_float(im_str!("Element Mutation Rate"),
+                                  &mut state.global_element_mutation,
+                                  0.0,
+                                  1.0)
+                    .build();
+
+            }
+
+
+            // ui.separator();
+            // let mouse_pos = ui.imgui().mouse_pos();
+            // ui.text(im_str!("Mouse Position: ({:.1},{:.1})", mouse_pos.0, mouse_pos.1));
+        })
+}
+
+
--- hypernsga-gui/src/main.rs
+++ hypernsga-gui/src/main.rs
@@ -20,11 +20,10 @@ use hypernsga::prob::Prob;
 use hypernsga::weight::{WeightPerturbanceMethod, WeightRange};
 use hypernsga::substrate::{Node, SubstrateConfiguration, Position, Position3d};
 use nsga2::selection::SelectNSGPMod;
-use nsga2::population::{UnratedPopulation, RankedPopulation};
+use nsga2::population::{UnratedPopulation};
 use std::f64::INFINITY;
 use std::env;
 
-use imgui::*;
 use self::support::Support;
 use glium::Surface;
 use std::io::Write;
@@ -33,6 +32,7 @@ use std::cmp::Ordering;
 pub use vertex::Vertex;
 use render_graph::render_graph;
 use render_cppn::render_cppn;
+use imgui_ui::gui;
 
 mod support;
 mod viz_network_builder;
@@ -41,6 +41,7 @@ mod shaders;
 mod substrate_configuration;
 mod render_graph;
 mod render_cppn;
+mod imgui_ui;
 
 const CLEAR_COLOR: (f32, f32, f32, f32) = (1.0, 1.0, 1.0, 1.0);
 
@@ -169,7 +170,7 @@ impl<'a, W: Write> NetworkBuilder for DotNetworkBuilder<'a, W> {
 
 
 #[derive(Debug)]
-enum ViewMode {
+pub enum ViewMode {
     BestDetailed,
     CppnOverview,
     GraphOverview,
@@ -178,33 +179,33 @@ enum ViewMode {
 
 #[derive(Debug)]
 pub struct State {
-    iteration: usize,
-    best_fitness: f64,
-    running: bool,
-    mu: i32,
-    lambda: i32,
-    k: i32,
-    mutate_add_node: i32,
-    mutate_drop_node: i32,
-    mutate_modify_node: i32,
-    mutate_connect: i32,
-    mutate_disconnect: i32,
-    mutate_symmetric_join: i32,
-    mutate_symmetric_fork: i32,
-    mutate_symmetric_connect: i32,
-    mutate_weights: i32,
+    pub iteration: usize,
+    pub best_fitness: f64,
+    pub running: bool,
+    pub mu: i32,
+    pub lambda: i32,
+    pub k: i32,
+    pub mutate_add_node: i32,
+    pub mutate_drop_node: i32,
+    pub mutate_modify_node: i32,
+    pub mutate_connect: i32,
+    pub mutate_disconnect: i32,
+    pub mutate_symmetric_join: i32,
+    pub mutate_symmetric_fork: i32,
+    pub mutate_symmetric_connect: i32,
+    pub mutate_weights: i32,
 
-    mutate_element_prob: f32,
-    nsgp_objective_eps: f32,
-    weight_perturbance_sigma: f32,
-    link_weight_range: f32,
-    link_weight_creation_sigma: f32,
+    pub mutate_element_prob: f32,
+    pub nsgp_objective_eps: f32,
+    pub weight_perturbance_sigma: f32,
+    pub link_weight_range: f32,
+    pub link_weight_creation_sigma: f32,
 
-    best_fitness_history: Vec<(usize, f64)>,
+    pub best_fitness_history: Vec<(usize, f64)>,
     // crossover_weights: 0,
-    nm_edge_score: bool,
-    nm_iters: i32,
-    nm_eps: f32,
+    pub nm_edge_score: bool,
+    pub nm_iters: i32,
+    pub nm_eps: f32,
 
     pub rotate_substrate_x: f32,
     pub rotate_substrate_y: f32,
@@ -213,29 +214,29 @@ pub struct State {
     pub scale_substrate_y: f32,
     pub scale_substrate_z: f32,
 
-    link_expression_min: f32,
-    link_expression_max: f32,
+    pub link_expression_min: f32,
+    pub link_expression_max: f32,
 
-    recalc_fitness: bool,
+    pub recalc_fitness: bool,
 
-    objectives_use_behavioral: bool,
-    objectives_use_cct: bool,
-    objectives_use_age: bool,
-    objectives_use_saturation: bool,
-    objectives_use_complexity: bool,
+    pub objectives_use_behavioral: bool,
+    pub objectives_use_cct: bool,
+    pub objectives_use_age: bool,
+    pub objectives_use_saturation: bool,
+    pub objectives_use_complexity: bool,
 
-    action: Action,
-    view: ViewMode,
+    pub action: Action,
+    pub view: ViewMode,
 
-    global_mutation_rate: f32,
-    global_element_mutation: f32,
+    pub global_mutation_rate: f32,
+    pub global_element_mutation: f32,
 
-    auto_reset: i32,
-    auto_reset_enable: bool,
-    auto_reset_counter: usize,
+    pub auto_reset: i32,
+    pub auto_reset_enable: bool,
+    pub auto_reset_counter: usize,
 
-    stop_when_fitness_above: f32,
-    enable_stop: bool,
+    pub stop_when_fitness_above: f32,
+    pub enable_stop: bool,
 }
 
 struct EvoConfig {
@@ -246,248 +247,10 @@ struct EvoConfig {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Action {
+pub enum Action {
     None,
     ExportBest,
     ResetNet,
-}
-
-fn gui<'a>(ui: &Ui<'a>, state: &mut State, population: &RankedPopulation<G, Fitness>) {
-    ui.window(im_str!("Evolutionary Graph Optimization"))
-        .size((300.0, 100.0), ImGuiSetCond_FirstUseEver)
-        .build(|| {
-            // if ui.collapsing_header(im_str!("General")).build() {
-            ui.text(im_str!("Iteration: {}", state.iteration));
-            ui.text(im_str!("Best Fitness: {:.3}", state.best_fitness));
-            ui.separator();
-
-            if state.running {
-                if ui.small_button(im_str!("STOP")) {
-                    state.running = false;
-                }
-            } else {
-                if ui.small_button(im_str!("START")) {
-                    state.running = true;
-                }
-            }
-
-            if ui.collapsing_header(im_str!("General")).build() {
-                if ui.small_button(im_str!("Recalc Fitness")) {
-                    state.recalc_fitness = true;
-                }
-                if ui.small_button(im_str!("Export Best")) {
-                    state.action = Action::ExportBest;
-                }
-                if ui.small_button(im_str!("Reset")) {
-                    state.action = Action::ResetNet;
-                }
-
-                ui.slider_float(im_str!("Stop when Fitness"),
-                                  &mut state.stop_when_fitness_above,
-                                  0.9,
-                                  1.0)
-                    .build();
-                ui.checkbox(im_str!("Enable Autoreset"), &mut state.auto_reset_enable);
-                ui.slider_int(im_str!("Autoreset after"),
-                                &mut state.auto_reset,
-                                100,
-                                10000)
-                    .build();
-                ui.text(im_str!("Autoreset counter: {}", state.auto_reset_counter));
-
-                let views = &[im_str!("detailed"),
-                              im_str!("multi cppn"),
-                              im_str!("multi graph"),
-                              im_str!("overview")];
-                let mut current: i32 = match state.view {
-                    ViewMode::BestDetailed => 0,
-                    ViewMode::CppnOverview => 1,
-                    ViewMode::GraphOverview => 2,
-                    ViewMode::Overview => 3,
-                };
-                if ui.combo(im_str!("view"), &mut current, views, 4) {
-                    if current == 0 {
-                        state.view = ViewMode::BestDetailed;
-                    } else if current == 1 {
-                        state.view = ViewMode::CppnOverview;
-                    } else if current == 2 {
-                        state.view = ViewMode::GraphOverview;
-                    } else if current == 3 {
-                        state.view = ViewMode::Overview;
-                    }
-                }
-            }
-
-            if ui.collapsing_header(im_str!("Population Metrics")).build() {
-                let best_domain = population.individuals()
-                    .iter()
-                    .max_by_key(|ind| (ind.fitness().domain_fitness * 1_000_000.0) as usize)
-                    .unwrap();
-                let worst_domain = population.individuals()
-                    .iter()
-                    .min_by_key(|ind| (ind.fitness().domain_fitness * 1_000_000.0) as usize)
-                    .unwrap();
-                ui.text(im_str!("Best Domain Fitness: {:.3}",
-                                best_domain.fitness().domain_fitness));
-                ui.text(im_str!("Age of Best: {}", best_domain.genome().age(state.iteration)));
-
-                ui.text(im_str!("Worst Domain Fitness: {:.3}",
-                                worst_domain.fitness().domain_fitness));
-                ui.text(im_str!("Age of Worst: {}",
-                                worst_domain.genome().age(state.iteration)));
-            }
-
-            if ui.collapsing_header(im_str!("History")).build() {
-                let fitness_histogram: Vec<_> =
-                    state.best_fitness_history.iter().map(|&(_i, f)| f as f32).collect();
-                PlotLines::new(im_str!("performance"), &fitness_histogram)
-                    .overlay_text(im_str!("Domain Fitness"))
-                    .graph_size(ImVec2::new(400.0, 50.0))
-                    .scale_min(0.0)
-                    .scale_max(1.0)
-                    .build();
-            }
-            if ui.collapsing_header(im_str!("Objectives")).build() {
-                ui.checkbox(im_str!("Behavioral Diversity"),
-                            &mut state.objectives_use_behavioral);
-                ui.checkbox(im_str!("Connection Cost"), &mut state.objectives_use_cct);
-                ui.checkbox(im_str!("Age Diversity"), &mut state.objectives_use_age);
-                ui.checkbox(im_str!("Saturation"), &mut state.objectives_use_saturation);
-                ui.checkbox(im_str!("Complexity"), &mut state.objectives_use_complexity);
-            }
-            if ui.collapsing_header(im_str!("Population Settings")).build() {
-                ui.slider_int(im_str!("Population Size"), &mut state.mu, state.k, 1000).build();
-                ui.slider_int(im_str!("Offspring Size"), &mut state.lambda, 1, 1000).build();
-            }
-
-            if ui.collapsing_header(im_str!("Selection")).build() {
-                ui.slider_int(im_str!("Tournament Size"), &mut state.k, 1, state.mu).build();
-                ui.slider_float(im_str!("NSGP Objective Epsilon"),
-                                  &mut state.nsgp_objective_eps,
-                                  0.0,
-                                  1.0)
-                    .build();
-            }
-
-            if ui.collapsing_header(im_str!("View")).build() {
-                ui.slider_float(im_str!("Rotate Substrate x"),
-                                  &mut state.rotate_substrate_x,
-                                  0.0,
-                                  360.0)
-                    .build();
-                ui.slider_float(im_str!("Rotate Substrate y"),
-                                  &mut state.rotate_substrate_y,
-                                  0.0,
-                                  360.0)
-                    .build();
-                ui.slider_float(im_str!("Rotate Substrate z"),
-                                  &mut state.rotate_substrate_z,
-                                  0.0,
-                                  360.0)
-                    .build();
-                ui.slider_float(im_str!("Scale Substrate x"),
-                                  &mut state.scale_substrate_x,
-                                  0.0,
-                                  1.0)
-                    .build();
-                ui.slider_float(im_str!("Scale Substrate y"),
-                                  &mut state.scale_substrate_y,
-                                  0.0,
-                                  1.0)
-                    .build();
-                ui.slider_float(im_str!("Scale Substrate z"),
-                                  &mut state.scale_substrate_z,
-                                  0.0,
-                                  1.0)
-                    .build();
-            }
-
-            if ui.collapsing_header(im_str!("CPPN")).build() {
-                ui.slider_float(im_str!("Link Expression Min"),
-                                  &mut state.link_expression_min,
-                                  -1.0,
-                                  1.0)
-                    .build();
-                ui.slider_float(im_str!("Link Expression Max"),
-                                  &mut state.link_expression_max,
-                                  -1.0,
-                                  1.0)
-                    .build();
-
-                ui.slider_float(im_str!("Link Weight Range (bipolar)"),
-                                  &mut state.link_weight_range,
-                                  0.1,
-                                  5.0)
-                    .build();
-                ui.slider_float(im_str!("Link Weight Creation Sigma"),
-                                  &mut state.link_weight_creation_sigma,
-                                  0.01,
-                                  1.0)
-                    .build();
-                ui.slider_float(im_str!("Weight Perturbance Sigma"),
-                                  &mut state.weight_perturbance_sigma,
-                                  0.0,
-                                  1.0)
-                    .build();
-            }
-
-            if ui.collapsing_header(im_str!("Neighbor Matching")).build() {
-                ui.checkbox(im_str!("Edge Weight Scoring"), &mut state.nm_edge_score);
-                ui.slider_int(im_str!("Iterations"), &mut state.nm_iters, 1, 1000).build();
-                ui.slider_float(im_str!("Eps"), &mut state.nm_eps, 0.0, 1.0).build();
-            }
-
-            if ui.collapsing_header(im_str!("Mutation")).build() {
-                ui.slider_float(im_str!("Mutation Rate"),
-                                  &mut state.mutate_element_prob,
-                                  0.0,
-                                  1.0)
-                    .build();
-                ui.slider_int(im_str!("Weights"), &mut state.mutate_weights, 1, 100).build();
-                ui.slider_int(im_str!("Add Node"), &mut state.mutate_add_node, 0, 100).build();
-                ui.slider_int(im_str!("Drop Node"), &mut state.mutate_drop_node, 0, 100).build();
-                ui.slider_int(im_str!("Modify Node"),
-                                &mut state.mutate_modify_node,
-                                0,
-                                100)
-                    .build();
-                ui.slider_int(im_str!("Connect"), &mut state.mutate_connect, 0, 100).build();
-                ui.slider_int(im_str!("Disconnect"), &mut state.mutate_disconnect, 0, 100).build();
-                ui.slider_int(im_str!("Sym Join"),
-                                &mut state.mutate_symmetric_join,
-                                0,
-                                100)
-                    .build();
-                ui.slider_int(im_str!("Sym Fork"),
-                                &mut state.mutate_symmetric_fork,
-                                0,
-                                100)
-                    .build();
-                ui.slider_int(im_str!("Sym Connect"),
-                                &mut state.mutate_symmetric_connect,
-                                0,
-                                100)
-                    .build();
-            }
-            if ui.collapsing_header(im_str!("Global Mutation")).build() {
-                ui.slider_float(im_str!("Global Mutation Rate"),
-                                  &mut state.global_mutation_rate,
-                                  0.0,
-                                  1.0)
-                    .build();
-                ui.slider_float(im_str!("Element Mutation Rate"),
-                                  &mut state.global_element_mutation,
-                                  0.0,
-                                  1.0)
-                    .build();
-
-            }
-
-
-            // ui.separator();
-            // let mouse_pos = ui.imgui().mouse_pos();
-            // ui.text(im_str!("Mouse Position: ({:.1},{:.1})", mouse_pos.0, mouse_pos.1));
-        })
 }
 
 
