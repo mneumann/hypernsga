b7e6bbe997d254a915eb9ec7ba76cba9ddc8a29b
Michael Neumann <mneumann@ntecs.de>
2016-08-18 10:57:39 +0200

    Factor out substrate configuration

--- hypernsga-gui/src/main.rs
+++ hypernsga-gui/src/main.rs
@@ -18,7 +18,7 @@ use hypernsga::fitness::{Fitness, DomainFitness};
 use hypernsga::mating::MatingMethodWeights;
 use hypernsga::prob::Prob;
 use hypernsga::weight::{WeightPerturbanceMethod, WeightRange};
-use hypernsga::substrate::{Node, NodeSet, Substrate, SubstrateConfiguration, Position, Position3d};
+use hypernsga::substrate::{Node, SubstrateConfiguration, Position, Position3d};
 use hypernsga::distribute::DistributeInterval;
 use nsga2::selection::{SelectNSGPMod};
 use nsga2::population::{UnratedPopulation, RankedPopulation};
@@ -39,6 +39,7 @@ mod support;
 mod viz_network_builder;
 mod vertex;
 mod shaders;
+mod substrate_configuration;
 
 use viz_network_builder::VizNetworkBuilder;
 
@@ -684,69 +685,12 @@ fn gui<'a>(ui: &Ui<'a>, state: &mut State, population: &RankedPopulation<G, Fitn
         };
 
         // XXX
-        let mut substrate: Substrate<Position3d, Neuron> = Substrate::new();
         let node_count = domain_fitness_eval.target_graph_node_count();
         println!("{:?}", node_count);
 
-        let input_nodeset = NodeSet::single(0);
-        let hidden_nodeset = NodeSet::single(1);
-        let output_nodeset = NodeSet::single(2);
+        let substrate_config = substrate_configuration::substrate_configuration(&node_count);
 
-        let nodeset_links = &[(input_nodeset, hidden_nodeset), (hidden_nodeset, output_nodeset), (input_nodeset, output_nodeset),
-        //(output_nodeset, input_nodeset)
-        ];
-
-        //let nodeset_links = &[(input_nodeset, hidden_nodeset), (hidden_nodeset, output_nodeset),
-        //(output_nodeset, input_nodeset)
-        //];
-
-
-        // Input layer
-        {
-            let z = -1.0;
-            //let z = 0.0;
-            //let y = 0.0;
-            for x in DistributeInterval::new(node_count.inputs, -1.0 * node_count.inputs as f64, 1.0 * node_count.inputs as f64) {
-            //for x in DistributeInterval::new(node_count.inputs, -1.0, 1.0) {
-                let y = 0.0; //0.1 * (1.0 - x.powi(8));
-                substrate.add_node(Position3d::new(x, y, z),
-                Neuron::Input,
-                input_nodeset);
-            }
-        }
-
-        // Hidden
-        {
-            let z = 0.0;
-            //let y = 0.0;
-            for x in DistributeInterval::new(node_count.hidden, -1.0 * node_count.hidden as f64, 1.0 * node_count.hidden as f64) {
-            //for x in DistributeInterval::new(node_count.hidden, -1.0, 1.0) {
-                //let y = (1.0 - x.powi(8));
-                let y = 0.0; //-1.0;
-                substrate.add_node(Position3d::new(x, y, z),
-                Neuron::Hidden,
-                hidden_nodeset);
-            }
-        }
-
-        // Outputs
-        {
-            let z = 1.0;
-            //let z = 1.0;
-            //let y = 0.0;
-            //let mut z = DistributeInterval::new(node_count.outputs, -0.1, 0.1);
-            for x in DistributeInterval::new(node_count.outputs, -1.0 * node_count.outputs as f64, 1.0 * node_count.outputs as f64) {
-            //for x in DistributeInterval::new(node_count.outputs, -1.0, 1.0) {
-                //let y = -0.1 * (1.0 - x.powi(8));
-                let y = 0.0;
-                //substrate.add_node(Position3d::new(x, y, -z.next().unwrap()),
-                substrate.add_node(Position3d::new(x, y, z),
-                Neuron::Output,
-                output_nodeset);
-            }
-        }
-
-        let mut evo_config = EvoConfig {
+       let mut evo_config = EvoConfig {
             mu: 100,
             lambda: 100,
             k: 2,
@@ -809,7 +753,6 @@ fn gui<'a>(ui: &Ui<'a>, state: &mut State, population: &RankedPopulation<G, Fitn
 
         let mut expression = Expression { link_expression_range: (0.1, 0.5) };
 
-        let substrate_config = substrate.to_configuration(nodeset_links);
 
         // create `generation 0`
         let mut parents = {
